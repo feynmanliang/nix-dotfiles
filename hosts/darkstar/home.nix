ea0b40816a52672f6d18e4bb706ed904d80d39a5
Feynman Liang <feynman.liang@gmail.com>
2023-03-19 12:20:27 -0700

    Configure nvim

--- hosts/darkstar/home.nix
+++ hosts/darkstar/home.nix
@@ -12,58 +12,7 @@ in
     ])
     ++ (with mypkgs; [ nvchad ]);
 
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      plugins = with pkgs.vimPlugins; [ 
-          vim-nix
-          plenary-nvim
-          gruvbox-material
-          {
-              plugin = impatient-nvim;
-              config = "lua require('impatient')";
-          }
-          {
-              plugin = lualine-nvim;
-              config = "lua require('lualine').setup()";
-          }
-          {
-              plugin = telescope-nvim;
-              config = "lua require('telescope').setup()";
-          }
-          {
-              plugin = indent-blankline-nvim;
-              config = "lua require('indent_blankline').setup()";
-          }
-          {
-              plugin = nvim-lspconfig;
-              config = ''
-                  lua << EOF
-                  require('lspconfig').rust_analyzer.setup{}
-                  require('lspconfig').sumneko_lua.setup{}
-                  require('lspconfig').rnix.setup{}
-                  require('lspconfig').zk.setup{}
-                  EOF
-              '';
-          }
-          {
-              plugin = nvim-treesitter;
-              config = ''
-              lua << EOF
-              require('nvim-treesitter.configs').setup {
-                  highlight = {
-                      enable = true,
-                      additional_vim_regex_highlighting = false,
-                  },
-              }
-              EOF
-              '';
-          }
-      ];
-    };
+    programs.neovim = import ./nvim.nix { pkgs = pkgs; };
   
     programs.tmux = {
       enable = true;
